7976532d71e07b3d5306e5815772ad67c3c063a7
Celso Trabalho <celsortc@outlook.com>
2025-11-24 16:49:05 -0300

    txt ajustes adicionado, correção de bug visual no css e acerto da lógica de segundos para minutos

--- script.js
+++ script.js
@@ -3,26 +3,37 @@ const pausar = document.querySelector(".pausar");
 const reiniciar = document.querySelector(".reiniciar");
 const milisegundos = document.querySelector(".milisecs");
 const segundos = document.querySelector(".segundos");
+const minutos = document.querySelector(".minutos");
 
 var interval;
 
 iniciar.onclick = () => {
   var mili = +milisegundos.innerHTML;
-
+  segundos.innerHTML = 55;
   interval = setInterval(() => {
-    mili += 10;
-
-    milisegundos.innerHTML = formataNumero(mili);
-    if (mili >= 1000) {
+    if (mili < 990) {
+      mili += 10;
+      milisegundos.innerHTML = formataNumero(mili);
+    } else if (mili === 990) {
       mili = 0;
-      var segs = +segundos.innerHTML;
-      segs++;
+      var sec = +segundos.innerHTML;
+      sec++;
+      segundos.innerHTML = formataNumero(sec);
+      console.log(typeof sec);
+    } else if (mili === 990 && sec === 59) {
+      segundos.innerHTML = "00";
 
-      segundos.innerHTML = formataNumero(segs);
+      var min = +minutos.innerHTML;
+      min++;
+      minutos.innerHTML = formataNumero(min);
     }
-  }, 10);
+  }, 1);
 };
 
 function formataNumero(numero) {
-  return numero.toString().padStart(2, "0").slice(0, 2);
+  if (numero < 100) {
+    return numero.toString().padStart(2, "0");
+  } else {
+    return numero.toString().slice(0, 2);
+  }
 }
